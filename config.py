881d33f799ac6ca9b37467121112f523a1e264e3
liu-ruhao <53904786+liu-ruhao@users.noreply.github.com>
2022-03-11 11:00:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+input_config={'input_name':'universe',#检索内容
+              'path':'D:\youtube_download',#路径
+              'step':0,#YouTube网页下滑次数
+              'split':True,#True#False#是否分割视频
+              'kill':True,#True#False#是否删除下载的原视频
+              'zip':True,#True#False#是否压缩视频文件夹
+              'max_num':5,#限制下载视频数量
+              'ban_index_1':'StevenUniverse',#屏蔽关键词1
+              'ban_index_2':'復活邪神Re',#屏蔽关键词2
+              'time':180,#切割视频的时间间隔，比如把一个视频切割成每段180s的多个视频
+              'skip1':-1,#测试时跳过太大的视频
+              'skip2':-1,
+              'skip3':-1,
+              'remake':False#True#False#是否删除result以重来
+              }
+
